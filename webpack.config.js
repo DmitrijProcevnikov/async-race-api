52fb83a86312f5a47118c38a3236d69d5524eb39
DmitrijProcevnikov <dmitrijprocevnikov@gmail.com>
2023-01-20 00:40:47 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+      },
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',            
+        }),
+    ],
+}
